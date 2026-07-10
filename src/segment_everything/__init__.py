3c3a6634c875c406133c92004eceaf2de3fdb0db
bnorthan <bnorthan@gmail.com>
2026-07-10 10:35:44 -0400

    Bump version
    ... and read __version__ from toml.

--- src/segment_everything/__init__.py
+++ src/segment_everything/__init__.py
@@ -1,5 +1,14 @@
 """Utilities for rendering and training prompt-based and overlapping segmentation models (like SAM). """
 
-__version__ = '0.1.0'
+try:
+    from importlib.metadata import version, PackageNotFoundError
+except ImportError:
+    from importlib_metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("segment-everything")
+except PackageNotFoundError:
+    __version__ = "unknown"
+
 __author__ = "Brian Northan"
 __email__ = "bnorthan@gmail.com"
